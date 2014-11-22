b4056029e35fd44307fc5a46b27463fc1bab83bc
David Hugh-Jones <davidhughjones@gmail.com>
2014-11-22 11:05:44 +0000

    tester using RCurl

---
+++ tester.R
@@ -0,0 +1,14 @@
+library(RCurl)
+
+N <- 20
+while (TRUE) {
+  for (i in 1:N) getURL(paste0("http://54.194.17.233/betr/betr/client-", i))
+  Sys.sleep(sample(1:5))
+}
+
+# plan:
+# create a test case:
+# Simplify experiment1 to bare minimum (brumdebug branch)
+# Simplify betr (brumdebug branch)
+# Reliably reproduce bug
+# Record what does/doesn't trigger it

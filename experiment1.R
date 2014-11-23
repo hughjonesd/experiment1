13e6c30f80668da1597aef542dadffc5bae1de24
David Hugh-Jones <davidhughjones@gmail.com>
2014-11-23 01:15:24 +0000

    name for experiment

--- experiment1.R
+++ experiment1.R
@@ -41,7 +41,7 @@ ready_fn <- function() {
   globals <<- NA
 }
 
-expt <- experiment(N=N, clients_in_url=ciu, on_ready=ready_fn, 
+expt <- experiment(N=N, clients_in_url=ciu, on_ready=ready_fn, name="brum1", 
       seed=seed, randomize_ids=TRUE, autostart=TRUE, client_refresh=1)
 
 s_consent <-text_stage(page=b_brew("consent.brew"), wait=TRUE, name="Consent")

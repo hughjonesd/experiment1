2a15e7b031af6d4dd9799ff387d10a207215a142
David Hugh-Jones <davidhughjones@gmail.com>
2014-11-23 03:01:45 +0000

    remove name, it broke our urls :-(

--- experiment1.R
+++ experiment1.R
@@ -45,7 +45,7 @@ ready_fn <- function() {
   timeout <<- NA
 }
 
-expt <- experiment(N=N, clients_in_url=ciu, on_ready=ready_fn, name="brum1", 
+expt <- experiment(N=N, clients_in_url=ciu, on_ready=ready_fn, 
       seed=seed, randomize_ids=TRUE, autostart=TRUE, client_refresh=1)
 
 s_consent <-text_stage(page=b_brew("consent.brew"), wait=TRUE, name="Consent")

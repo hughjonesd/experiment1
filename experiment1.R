4e3c2c92da57212691af386b6c4c2fbd3bf6dd74
David Hugh-Jones <davidhughjones@gmail.com>
2014-11-23 02:49:35 +0000

    fix unfixed conflict

--- experiment1.R
+++ experiment1.R
@@ -244,20 +244,6 @@ s_final_calcs <- program(run="first",
 s_show_result <- text_stage(page=b_brew("results.brew"), name="Final results")
 
 add_stage(expt, checkpoint(),
-<<<<<<< HEAD
-#      s_consent, checkpoint(), s_rules, checkpoint(), s_instr,
-#      checkpoint(), s_instr2, checkpoint(), s_instr3,
-      period(wait_for="none"), s_dict, s_prog_dict, 
-      period(wait_for="none"), s_ug, checkpoint("none"), s_ug_cont, s_prog_ug,
-      period(wait_for="none"), s_ig, s_prog_ig,
-      period(wait_for="none"), s_friendsintro, s_friends, 
-      period(wait_for="none"), s_friends, 
-      period(wait_for="none"), s_friends, 
-      period(wait_for="none"), s_friends, 
-      period(wait_for="none"), s_friends, 
-      period(wait_for="none"), s_friends, 
-      period(wait_for="none"), s_myfriends, s_friends_like, s_qnaire,
-=======
       s_consent, checkpoint(), s_rules, checkpoint(), s_instr,
       checkpoint(), s_instr2, checkpoint(), s_instr3,
       period(wait_for="none"), s_prog_timer, s_dict, s_prog_dict, 
@@ -272,7 +258,6 @@ add_stage(expt, checkpoint(),
       period(wait_for="none"), s_prog_timer, s_friends, 
       period(wait_for="none"), s_prog_timer, s_myfriends, s_prog_timer, 
       s_friends_like, s_prog_timer, s_qnaire,
->>>>>>> master
       period(wait_for="all"), s_final_calcs, s_show_result
       )
 

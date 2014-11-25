3efe3b018c78c9db1bc2d6253eb92dce770e6cda
David Hugh-Jones <davidhughjones@gmail.com>
2014-11-25 12:46:18 +0000

    guess stage

--- experiment1.R
+++ experiment1.R
@@ -25,6 +25,9 @@ ready_fn <- function() {
         accept2=NA, accepted2=NA, hchoice=NA, coinflip=NA, coinflip.real=NA,
         profit=NA, ngroups=NA, friends=NA, myfriends=NA, friendslike=NA,
         myname=NA, myname2=NA,
+        guessname1=NA, guessname2=NA, guessname3=NA,
+        guess1=NA, guess2=NA, guess3=NA,
+        paidguess=NA,
         rank=sample(N), role=NA, pair=NA, stringsAsFactors=FALSE)
   globals <<- NA
   timeout <<- NA
@@ -37,8 +40,49 @@ auth_fn <- function (ip, params, cookies) {
   return(FALSE)
 }
 
+
 expt <- experiment(N=N, clients_in_url=ciu, on_ready=ready_fn, auth=auth_fn,
       seed=seed, randomize_ids=TRUE, autostart=TRUE, client_refresh=5)
+
+# CHECK FUNCTIONS ===================
+
+
+nocheck <- function(...) NULL
+
+namecheck <- function(title, value, id, period, params) {
+  if (value == "" && params$myname2 == "") return("Please enter your name") 
+  return(NULL)
+}
+
+frcheck <- function(title, values, id, period, params) {
+  if (length(values)==1) return("Please tick more than one checkbox to show
+    who else hangs around with this pupil.")
+  if (all(values %in% classnames)) return(NULL)
+  wrong <- setdiff(frs, classnames)
+  return(paste("Unrecognized pupil names: ", paste(wrong, collapse=", "), 
+    sep=""))
+}
+
+frlikecheck <- function(title, values, id, period, params) {
+  if (length(values) > 3) return("Please only tick up to 3 pupils")
+  if (all(values %in% classnames)) return(NULL)
+  wrong <- setdiff(frs, classnames)
+  return(paste("Unrecognized pupil names: ", paste(wrong, collapse=", "), 
+    sep=""))
+}
+
+pupilcheck <- function(title, values, id, period, params) {
+  if (all(values %in% classnames)) return(NULL)
+  wrong <- setdiff(frs, classnames)
+  return(paste("Unrecognized pupil names: ", paste(wrong, collapse=", "), 
+    sep=""))
+}
+
+
+
+
+
+# STAGES ================
 
 s_consent <-text_stage(page=b_brew("consent.brew"), wait=TRUE, name="Consent")
 s_rules <-  text_stage(page=b_brew("rules.brew"), wait=TRUE, name="Rules")
@@ -77,7 +121,6 @@ s_prog_dict <- program(run="last",
     }
   }, 
   name="DG profit calculations")
-
 
 s_instr_ug <- text_stage(page=b_brew("instr_ug.brew"), wait=TRUE, 
       name="Stage 2 Part 1 Instrns")
@@ -154,10 +197,69 @@ s_prog_ig <- program(run="last",
       mydf$profit[pd & mydf$role=="B" & mydf$pair==pr] <<- 100 *
         sum(mydf$hchoice[pd & mydf$role=="A" & mydf$pair==pr] == "give")
     }
+    
   },
   name="IG profit calculations")
 
+
+write_payment_data <- function() {
+  globals <<- dcast(melt(mydf[,c("id", "period", "profit")], id=1:2), 
+    id ~ period)
+  globals$totalprofit <<- rowSums(globals[-1], na.rm=TRUE)
+  globals$totalprofit <<- globals$totalprofit
+  globals <<- merge_subjects(expt, globals)[,c("seat", "id", "IP", "client",
+    "totalprofit")]
+  globals <<- globals[order(globals$seat, globals$id),]
+  payfile <- paste0("session-", sessno, "-paydata.csv")
+  write.csv(globals, file=payfile)
+  cat("Payment data written to", sQuote(payfile), ".\n")
+  cat("Look at 'globals' to display it now.\n")
+}
+
 s_q_intro <- text_stage(page=b_brew("q_intro.brew"), name="Questionnaire Intro")
+
+s_qnaire <- form_stage(page=b_brew("qnaire.brew"),
+      fields=list(myname=namecheck, myname2=nocheck),
+      titles=list(myname="Name", myname2=""),
+      data_frame="mydf",
+      name="Questionnaire: name")
+
+s_prog_prepare_guess <- program(run="first", 
+  fn=function(id, period, ...) {
+    pd <- mydf$period==period
+    mydf$myname[pd] <<- ifelse (nzchar(mydf$myname[pd]), mydf$myname[pd], mydf$myname2[pd])
+    mynames <- mydf$myname[pd]
+    l <- length(mynames)
+    mydf$guessname1[pd] <<- mynames[c(2:l, 1)]
+    mydf$guessname2[pd] <<- mynames[c(3:l, 1:2)]
+    mydf$guessname3[pd] <<- mynames[c(4:l, 1:3)]
+  },
+  name="Prepare guess names")
+
+s_guess <- form_stage(page=b_brew("guesses.brew"),
+      fields=list(guess1=is_one_of(0:10*10), guess2=is_one_of(0:10*10), 
+      guess3=is_one_of(0:10*10)),
+      titles=list(guess1="Guess 1", guess2="Guess 2", guess3="Guess 3"),
+      data_frame="mydf",
+      name="Questionnaire: guesses")
+
+s_prog_guess <- program(run="last", 
+  fn=function(id, period, ...) {
+    pd <- mydf$period==period
+    g <- sample(1:3, 1)
+    aim <- sapply(mydf$id[pd], function(id, g) {
+      guessfield <- paste0("guessname", g)
+      guessname <- mydf[pd & mydf$id==id, guessfield]
+      tgtid <- mydf$id[pd & mydf$myname == guessname]
+      tguess <- mydf$dict1[mydf$period==1 & mydf$id==tgtid]
+      return(tguess == mydf[,paste0("guess", g)])
+    }, g=g)
+    mydf$profit[pd] <<- mydf$profit[pd] + 50 * aim
+    mydf$paidguess[pd] <<- g
+  },
+  name="Calculate guess profits")
+
+s_prog_paydata <- program(run="last", write_payment_data,  name="Write payment data")
 
 s_friendsintro <-  form_stage(
   page=b_brew("friends_intro.brew"),
@@ -165,16 +267,6 @@ s_friendsintro <-  form_stage(
   titles=list(ngroups="Number of groups in your class"),
   data_frame="mydf", multi_params="paste",
   name="Questionnaire: friends intro")
-
-
-frcheck <- function(title, values, id, period, params) {
-  if (length(values)==1) return("Please tick more than one checkbox to show
-    who else hangs around with this pupil.")
-  if (all(values %in% classnames)) return(NULL)
-  wrong <- setdiff(frs, classnames)
-  return(paste("Unrecognized pupil names: ", paste(wrong, collapse=", "), 
-        sep=""))
-}
 
 frpagefn <- function(id, period, params, errors) {
   ng <- mydf$ngroups[mydf$id==id & ! is.na(mydf$ngroups)]
@@ -194,59 +286,17 @@ s_friends <-  form_stage(
       data_frame="mydf", multi_params="paste",
       name="Questionnaire: friendship networks")
 
-myfrcheck <- function(title, values, id, period, params) {
-  if (all(values %in% classnames)) return(NULL)
-  wrong <- setdiff(frs, classnames)
-  return(paste("Unrecognized pupil names: ", paste(wrong, collapse=", "), 
-    sep=""))
-}
-
-nocheck <- function(...) NULL
-
 s_myfriends <- form_stage(page=b_brew("myfriends.brew"),
-      fields=list(myfriends=myfrcheck),
+      fields=list(myfriends=pupilcheck),
       titles=list(myfriends="My Friends"),
       data_frame="mydf", multi_params="paste",
       name="Questionnaire: my friends")
-
-frlikecheck <- function(title, values, id, period, params) {
-  if (length(values) > 3) return("Please only tick up to 3 pupils")
-  if (all(values %in% classnames)) return(NULL)
-  wrong <- setdiff(frs, classnames)
-  return(paste("Unrecognized pupil names: ", paste(wrong, collapse=", "), 
-    sep=""))
-}
 
 s_friends_like <- form_stage(page=b_brew("friendslike.brew"),
       fields=list(friendslike=frlikecheck),
       titles=list(friendslike="Friends I would like"),
       data_frame="mydf", multi_params="paste",
       name="Questionnaire: friends I'd like")
-
-namecheck <- function(title, value, id, period, params) {
-  if (value == "" && params$myname2 == "") return("Please enter your name") 
-  return(NULL)
-}
-
-s_qnaire <- form_stage(page=b_brew("qnaire.brew"),
-      fields=list(myname=namecheck, myname2=nocheck),
-      titles=list(myname="Name", myname2=""),
-      data_frame="mydf",
-      name="Questionnaire: other")
-
-write_payment_data <- function() {
-  globals <<- dcast(melt(mydf[,c("id", "period", "profit")], id=1:2), 
-    id ~ period)
-  globals$totalprofit <<- rowSums(globals[-1], na.rm=TRUE)
-  globals$totalprofit <<- globals$totalprofit
-  globals <<- merge_subjects(expt, globals)[,c("seat", "id", "IP", "client",
-    "totalprofit")]
-  globals <<- globals[order(globals$seat, globals$id),]
-  payfile <- paste0("session-", sessno, "-paydata.csv")
-  write.csv(globals, file=payfile)
-  cat("Payment data written to", sQuote(payfile), ".\n")
-  cat("Look at 'globals' to display it now.\n")
-}
 
 s_final_calcs <- program(run="first",
   function(...) {
@@ -267,7 +317,10 @@ add_stage(expt, checkpoint(),
       s_instr_ugcont, checkpoint(),
       s_prog_timer, s_ug_cont, s_prog_ug,
       period(wait_for="all"), s_instr_ig, checkpoint(), 
-      s_prog_timer, s_ig, s_prog_ig, s_q_intro,
+      s_prog_timer, s_ig, s_prog_ig, 
+      s_q_intro, 
+      s_prog_timer, s_qnaire, 
+      s_prog_timer, s_guess, s_prog_paydata,
       period(wait_for="none"), s_prog_timer, s_friendsintro, s_friends, 
       period(wait_for="none"), s_prog_timer, s_friends, 
       period(wait_for="none"), s_prog_timer, s_friends, 
@@ -275,7 +328,7 @@ add_stage(expt, checkpoint(),
     #  period(wait_for="none"), s_prog_timer, s_friends, 
     #  period(wait_for="none"), s_prog_timer, s_friends, 
       period(wait_for="none"), s_prog_timer, s_myfriends, s_prog_timer, 
-      s_friends_like, s_prog_timer, s_qnaire,
+      s_friends_like, 
       period(wait_for="all"), s_final_calcs, s_show_result
       )
 
